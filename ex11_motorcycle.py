aa9f4d6c94b67040066802b9704539b5f07fad42
Jason Smith <jason@smithss.org>
2013-05-19 17:22:22 -0500

    motorcycle extra credit

---
+++ ex11_motorcycle.py
@@ -0,0 +1,9 @@
+print "What type of motorcycle do you have?",
+m_type = raw_input()
+print "What brand of motorcycle do you have?",
+m_brand = raw_input()
+print "What model of motorcycle do you have?",
+m_model = raw_input()
+
+print "So, you have a {0} type, {1} brand, and {2} model.".format(m_type, 
+    m_brand, m_model)

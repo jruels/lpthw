2b58275db81413ba8419038a6a89f464774471c1
Jason Smith <jason@smithss.org>
2013-05-20 11:34:30 -0500

    created file using close()

---
+++ ex15_close.py
@@ -0,0 +1,21 @@
+# import argv module
+from sys import argv
+# assign script, and filename to CLI arguments
+script, filename = argv
+# assign 'txt' variable to open a file passed on command-line
+txt = open(filename)
+# print the output of the file.
+print "Here's your file {0!r}: ".format(filename)
+print txt.read()
+
+# close a file
+txt.close()
+# prompt for filename on command-line
+print "Type the filename again:"
+file_again = raw_input("> ")
+# assign txt_again to open the file that was provided in last step
+txt_again = open(file_again)
+# print out the file that was provided.
+print txt_again.read()
+
+txt_again.close()
